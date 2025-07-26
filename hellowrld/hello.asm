05b1a97fcdd5a3346a5e788606be797b18d7a151
RestoneAGX <77562037+Ayidana-Aboraah@users.noreply.github.com>
2025-07-26 06:50:28 -0400

    No more seg fault :]

--- hellowrld/hello.asm
+++ hellowrld/hello.asm
@@ -5,8 +5,11 @@ SECTION .text
 global _start
 
 _start:
-  mov edx, 13
-  mov ecx, msg
-  mov ebx, 1
-  mov eax, 4
-  int 80h
+  mov edx, 13  ; number of bytes to write - one for each letter plus 0Ah (line feed character)
+  mov ecx, msg ; move the memory address of our message string into ecx
+  mov ebx, 1   ; write to the STDOUT file
+  mov eax, 4   ; invoke SYS_WRITE (kernel opcode 4)
+  int 80h      ; interrupt
+  mov ebx, 0   ; no errors
+  mov eax, 1   ; exit
+  int 80h      ; interrupt and do the thing
