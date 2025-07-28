594ec02a38e50cd259ddaaa37d9439004cf7ace0
RestoneAGX <77562037+Ayidana-Aboraah@users.noreply.github.com>
2025-07-28 06:41:27 -0400

    Functions + Strlen

--- hellowrld/hello.asm
+++ hellowrld/hello.asm
@@ -5,7 +5,10 @@ SECTION .text
 global _start
 
 _start:
-  mov edx, 13  ; number of bytes to write - one for each letter plus 0Ah (line feed character)
+  ; now let's calculate our length before we send off the string
+  mov ebx, msg
+  call strlen
+
   mov ecx, msg ; move the memory address of our message string into ecx
   mov ebx, 1   ; write to the STDOUT file
   mov eax, 4   ; invoke SYS_WRITE (kernel opcode 4)
@@ -13,3 +16,18 @@ _start:
   mov ebx, 0   ; no errors
   mov eax, 1   ; exit
   int 80h      ; interrupt and do the thing
+
+strlen:
+  push ebx
+  mov edx, ebx ; edx will be the register we store our return value in
+
+loop:
+  cmp byte[edx], 0  ; check edx for string delimiter
+  jz finished       ; jump to finished if zero-flag has been triggered
+  inc edx           ; increment edx aka continue moving down the string
+  jmp loop          ; back to the beginning of the loop
+
+finished:
+  sub edx, ebx ; subtract the beginning address from our end address to get our length
+  pop ebx
+  ret
